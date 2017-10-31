21a1d0a4078ff9a61d88a6a673c09367d342fa08
YCHS <YCHS@EthanSdeMacBook.local>
2017-10-31 02:02:10 -0400

    homework3

---
+++ homework/homework3/123.js
@@ -0,0 +1,52 @@
+alert("Enjoy your 12mins Free trial! ")
+
+
+var sign = prompt("WHAT'S YOUR SIGN? (USING lowercase!)");
+
+if (sign.toLowerCase() == "aries") {
+  alert("Personality Aries is the first sign of the twelve zodiacs. It represents the beginning of all things and usually displays a strong desire to lead. Arians are adventurous, energetic, pioneering, and courageous. Arians always want to be on top. They tend to be frank and open but also self-centered and willful. Arians love adventure and almost have no fear. All is done with great enthusiasm which affects the people around them. Arians are loyal to family and friends. Aries is ruled by the forceful Mars. Arians are good athletes, doctors, explorers, soldiers, and leaders. Sometimes they like physical, emotional and mental extremes but Arians should be careful not to go too far with their extremism");
+}
+
+if (sign.toLowerCase() == "taurus") {
+  alert("Personality Taurus is the second sign of the zodiac, representing growth and development. Taurus is patient, reliable, warmhearted, loving, persistent, and determined. It does'nt like changes, given its reserved and practical nature.Taureans show a great interest in all physical matters. Taurus is faithful to friends and lovers.");
+}
+
+if (sign.toLowerCase() == "gemini") {
+  alert("Personality Gemini is the third sign of the zodiac, representing quickness of thought. Geminis are good communicators. They are active and clever. Geminis are adaptable, versatile,communicative, witty, intellectual, and eloquent. Sometime they can be pessimistic which is caused by their dual nature. Geminis like being part of a group. They always follow the rules of the game. Understanding and tolerant, they can be helpful in any situation. They usually liven up any social gathering with their ideas and verbal facility. Gemini is ruled by Mercury. Geminis can adapt to any situation, however they still try to keep their independence. Geminis like to travel and socialize. Geminis are good lawyers, politicians and public speakers.");
+}
+
+if (sign.toLowerCase() == "cancers") {
+  alert("Personality Cancers are protective, traditional, and emotional. Sometimes Cancers can be aggressive in getting what they want. They like extremes: they can be joyful and melancholic in a short period of time. The heart of their personality is sensitivity. They take everything verypersonally. Cancers give high value to their homes. Cancers do not like to be criticized or insulted. Cancers like to give away things. They are good lawyers, architects, designers, and painters.");
+}
+
+if (sign.toLowerCase() == "leo") {
+  alert("Personality Leos are like lions: energetic, free and independent. Leos like to get things done. They are excellent leaders because of their authoritative and confident character. They are very loyal to other people, even to their enemies. They like to socialize and give away presents. They like to buy fancy items. Leos like to get the attention of others. They can do many different jobs and learn easily. They always follow their principles so it is very unlikely to be surprised by a Leo. They also like children and are good entertainers. Leo is ruled by the Sun. Leos are optimistic because of their strong confidence in themselves. Leos are good leaders, managers, performers and negotiators.");
+}
+
+if (sign.toLowerCase() == "virgo") {
+  alert("Personality Virgo likes to attack back when confronted. Virgos need to receive a lot attention, adoration and gratitude. Virgos are accurate, competent and effective. They like rationality. Virgos like to help and solve problems quickly. They do not do it for compliments. Virgos are notwasteful. Being perfectionists, they like order and cleanliness in their home. Virgos change friends often. They have only a few real friends because they attribute special importance to friendship to which other people cannot commit. Virgo is ruled by Mercury. Virgos are very bright and educated. They are good scientists, teachers, analysts and planners.");
+}
+
+if (sign.toLowerCase() == "libra") {
+  alert("Personality Libra is very sociable and likes being a part of a group. Libras have gentle and refined personalities and like to be praised and pampered. Libras are perfectionists. They like to give a fair judgment when they hear all disputing sides. Libras are loyal, caring, nice, and adaptive. Libras are very trustworthy in friendships and relationships. Sometimes this makes them unreasonably possessive to other people. Libras like large, open spaces like halls and stadiums. They like to be out in nature. The sign is ruled by Venus. Libras also are very devoted to what they do and always try to get things done with excellence. Libras are good judges, managers, writers and social workers.");
+}
+
+if (sign.toLowerCase() == "scorpios") {
+  alert("Personality Scorpios have a very complicated personality. They are clever, uncompromising, calculating, and self-protective. Money is very important for them. Sometimes they have a SIXTH SENSE: they can feel things by intuition that other people cannot. They are very loyal and protective towards friends and loved ones. They would do anything to accomplish their goals. In relationships, they could be dangerously jealous. They are also passionate and caring. Scorpios are good teachers, actors, businessmen, accountants and sociologists.");
+}
+
+if (sign.toLowerCase() == "sagittarians") {
+  alert("Personality Sagittarians have an energetic and active personality. People covering this sign want to know everything and everyone. They also like to travel and to be close to nature.Sagittarians are optimistic, faithful, and honest. Other people often share their problems with Sagittarians, to seek help and understanding. Sagittarians are not very emotional. They adjust quickly to any environment. Sagittarians are good friends but still they keep their freedom and neutrality. Sagittarius is ruled by Jupiter. Sagittarius is very curious by nature. Such curiosity makes them well educated and intelligent people. SometimesSagittarians can be rebellious but they can quickly calm down and return to their ordinary nature. Sagittarians are good salesmen, scientists, researchers and analysts.");
+}
+
+if (sign.toLowerCase() == "capricorns") {
+  alert("Personality Capricorns never repeat their mistakes. They are very careful, slow, practical and thrifty. Their reserved personality can cause problems in their relationships. They are faithful to their family and loved ones. Their personality is also kind and affectionate but is suppressed by shyness. Trying to be perfect in everything they undertake, they often miss great opportunities because of lack of confidence in their abilities. Careful planning and insuring against any negative outcome is a must for all their actions. Capricorn is ruled by Saturn. Capricorn's nature can easily change when they are loved or respected by other people. Capricorns are good brokers, politicians, economists and bankers.");
+}
+
+if (sign.toLowerCase() == "Aquarians") {
+  alert("Personality Aquarians have a good sense of humor. They are gentle and caring and they are not very sociable. They prefer to be alone or with their closest friends. That's why Aquarius chooses its friends carefully. They like to receive compliments and are ready to forgive when harmed. They can easily entertain other people which makes them charming and wanted. Usually they are calm and sincere. However, when they get angry, they can be dangerous. Most of the time they overestimate the sincerity and the promises of their partners for which they regret later on. Aquarius is ruled by Saturn and Uranus. Aquarians are good workers and usually are paid well. Aquarians usually have many different hobbies and are creative and skillful in many areas. Aquarians are good computer scientists, doctors, pilots and writers.");
+}
+
+if (sign.toLowerCase() == "Pisces") {
+  alert("Personality Pisces are independent and mystical. They are sensitive and loving. They can easily adapt and change according to the situation. Pisces can handle many different tasks at once and be successful in all of them. Although trying to be independent, they like to have somebody above them to manage all the tasks they perform. Pisces can tolerate other people's behaviors; they are religious and pay strong attention to family and relationship values.");
+}
